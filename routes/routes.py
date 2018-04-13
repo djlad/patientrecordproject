8cb3048ab51c3678c3a9b4536fece62ce74b1854
dan <dynamicdan100@yahoo.com>
2018-04-12 21:20:37 -0400

    changed to static file structure

--- routes/routes.py
+++ routes/routes.py
@@ -2,14 +2,14 @@ from flask import Blueprint, render_template, request
 from models.patientmodel import PatientModel
 
 mainroutes = Blueprint("mainroutes", __name__)
-
+'''
 @mainroutes.route('/')
 def render_general_users_area():
     patient_model = PatientModel()
     patient_model.add_patient()
     #return render_template('general-users.html')
     return render_template('index.html')
-
+'''
 
 @mainroutes.route('/adduser')
 def add_user():
